bc49ad3379a767cdfd4d4a705292a3cb96522572
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-12-14 10:15:30 +1100

    Add brewfile

---
+++ brewfile.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+brew update
+brew upgrade
+
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap raggi/ale
+brew tap thoughtbot/formulae
+
+brew install cask
+brew install cmatrix
+brew install ctags
+brew install elixir
+brew install emacs
+brew install erlang
+brew install fontconfig
+brew install gcc48
+brew install gettext
+brew install ghostscript
+brew install git
+brew install hub
+brew install imagemagick
+brew install keybase
+brew install libksba
+brew install libxml2
+brew install libyaml
+brew install macvim
+brew install mercurial
+brew install phantomjs
+brew install postgresql
+brew install rcm
+brew install rust
+brew install sqlite
+brew install the_silver_searcher
+brew install tmux
+brew install tree
+brew install wget
