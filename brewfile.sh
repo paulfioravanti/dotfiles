fd80de0eb530fcd07c034811f5029976a5216b0a
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-12-14 13:09:44 +1100

    Add cask entries to brewfile

--- brewfile.sh
+++ brewfile.sh
@@ -36,3 +36,50 @@ brew install the_silver_searcher
 brew install tmux
 brew install tree
 brew install wget
+
+brew cask install 1password
+brew cask install alfred
+brew cask install audacity
+brew cask install balsamiq-mockups
+brew cask install bettertouchtool
+brew cask install calibre
+brew cask install chicken
+brew cask install colloquy
+brew cask install controllermate
+brew cask install dash
+brew cask install divvy
+brew cask install dropbox
+brew cask install duplicate-annihilator
+brew cask install filezilla
+brew cask install firefox
+brew cask install fitbit-connect
+brew cask install flip4mac
+brew cask install flowdock
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-japanese-ime
+brew cask install gpgtools
+brew cask install growlnotify
+brew cask install handbrake
+brew cask install iterm2
+brew cask install karabiner
+brew cask install little-snitch
+brew cask install omnigraffle
+brew cask install pgadmin3
+brew cask install picasa
+brew cask install screenflow
+brew cask install silverlight
+brew cask install simple-comic
+brew cask install skitch
+brew cask install skype
+brew cask install slack
+brew cask install smcfancontrol
+brew cask install sqlitebrowser
+brew cask install steam
+brew cask install sublime-text
+brew cask install teensy
+brew cask install unrarx
+brew cask install vlc
+brew cask install vox
+brew cask install wjoy
+brew cask install zipeg
