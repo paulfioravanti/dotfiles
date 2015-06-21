3c9006efd02862dc2c5755568ecd7e0e92789835
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-06-21 11:50:23 +1000

    Add rcrc rcup config file and run rcup with exclusions in the setup script as it won't be able to read config before it creates it

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Set up a new Mac to get up and running with development
-source `dirname $BASH_SOURCE`/brew/brewfile.sh
+DIR=`dirname $BASH_SOURCE`
+source $DIR/brew/brewfile.sh
 
+# Bootstrap rcup: the exclusions here are enumerated in the rcrc file so
+# you should be able to just run $ rcup when doing this again
+# Ignores the README and the brew directory for symlinking
+rcup -x README.md *:brew
