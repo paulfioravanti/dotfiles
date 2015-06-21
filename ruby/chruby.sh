20cc69704a54804804e5eb55f405faec98b00b8e
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-06-21 23:09:40 +1000

    Change reading in the rubies file to use a while loop due to the spaces present in the rubies.txt file.  If you use a for loop to read in the line, any spaces will be considered line delimiters by default: a while loop will ensure that the whole line of a file is read regardless of what's on the line

--- ruby/chruby.sh
+++ ruby/chruby.sh
@@ -5,9 +5,14 @@
 # get current directory
 CHRUBY_DIR=`dirname $BASH_SOURCE`
 
+# Use chruby and Ruby to generate a list of Rubies to install
 # $ chruby | ruby -n -e 'puts $_.gsub(/^[^\w]+|-p[0-9]+|\n/, "").gsub("-", " ")' > ~/.dotfiles/ruby/rubies.txt
-for ruby_version in `cat $CHRUBY_DIR/rubies.txt`; do
+#
+# This requires a while loop (as apposed to for loop used elsewhere) in the
+# dotfiles due to there being spaces in the rubies.txt files and we want to
+# read the file in whole line by line
+while read ruby_version; do
   ruby-install $ruby_version
-done
+done < $CHRUBY_DIR/rubies.txt
 
 chruby `cat ~/.ruby-version`
