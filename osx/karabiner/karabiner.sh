aabfb39d914766bc144cdf814c7c940e188e0bc7
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-07-01 20:07:30 +1000

    Update config for codeclimate, karabiner, and iterm2

--- osx/karabiner/karabiner.sh
+++ osx/karabiner/karabiner.sh
@@ -6,4 +6,8 @@ $cli set remap.return2controlL_return 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
+$cli set repeat.wait 32
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
 /bin/echo
