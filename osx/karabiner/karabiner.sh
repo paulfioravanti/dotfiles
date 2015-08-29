3c89f6f74678b86a86ecf085f154d521db498a59
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-08-29 12:29:45 +1000

    Update dotfiles config

--- osx/karabiner/karabiner.sh
+++ osx/karabiner/karabiner.sh
@@ -2,11 +2,11 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set repeat.wait 32
+/bin/echo -n .
 $cli set remap.return2controlL_return 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
-$cli set repeat.wait 32
 /bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .
