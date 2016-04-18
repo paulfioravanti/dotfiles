909fc504d0b7be3eea95c9a09313b39d4826a69f
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-04-18 11:47:34 +1000

    Specifically source .bashrc in config script otherwise chruby won't work

--- config.sh
+++ config.sh
@@ -4,6 +4,9 @@
 # Get current directory
 DOTFILES_DIR=$(dirname $BASH_SOURCE)
 
+# Get the aliases and functions from .bashrc settings
+# (specifically for chruby)
+source ~/.bashrc
 source $DOTFILES_DIR/ruby/rubies.sh
 source $DOTFILES_DIR/osx/brew/brews.sh
 source $DOTFILES_DIR/osx/karabiner/config.sh
