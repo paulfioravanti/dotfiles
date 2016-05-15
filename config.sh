3521232bf962e5ff7c620651079349ea68a77fce
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-05-15 17:53:19 +1000

    Only source rc file if using bash

--- config.sh
+++ config.sh
@@ -4,9 +4,11 @@
 # Get current directory
 DOTFILES_DIR=$(dirname ${BASH_SOURCE:-$0})
 
-# Get the aliases and functions from bashrc settings first,
-# specifically for chruby
-source $DOTFILES_DIR/bashrc
+if [ -n "$BASH_VERSION" ]; then
+  # Get the aliases and functions from bashrc settings first,
+  # specifically for chruby
+  source $DOTFILES_DIR/bashrc
+fi
 source $DOTFILES_DIR/ruby/rubies.sh
 source $DOTFILES_DIR/osx/brew/brews.sh
 source $DOTFILES_DIR/osx/karabiner/config.sh
