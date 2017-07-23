cfcc6917e5bcadbe7a670c4c8891846c1a597e62
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-07-24 09:53:04 +1000

    Add Elixir syntax colours for iex

--- iex.exs
+++ iex.exs
@@ -2,5 +2,12 @@ IEx.configure colors: [
   eval_info: [:yellow, :normal],
   eval_result: [:cyan, :normal],
   stack_app: [:green, :normal],
-  stack_info: [:white, :normal]
+  stack_info: [:normal],
+  syntax_colors: [
+    atom: :cyan,
+    boolean: :magenta,
+    nil: :magenta,
+    number: :yellow,
+    string: :green
+  ]
 ]
