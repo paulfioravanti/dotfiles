846d83ecab76fb81d30c7a0eddf413ac9a617e31
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-05-15 10:11:05 +1000

    Fix formatting

--- keyboards/ergodox_ez/keymaps/paulfioravanti/keymap.c
+++ keyboards/ergodox_ez/keymaps/paulfioravanti/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                           KC_BSPACE,      KC_DELETE, KC_END,
   // right hand
   KC_EQUAL,  KC_6,   KC_7,      KC_8,       KC_9,    KC_0,      KC_MINUS,
-  MO(MEDIA),       KC_Y,   KC_U,      KC_I,       KC_O,    KC_P,      KC_QUOTE,
+  MO(MEDIA), KC_Y,   KC_U,      KC_I,       KC_O,    KC_P,      KC_QUOTE,
              KC_H,   KC_J,      KC_K,       KC_L,    KC_SCOLON, RCTL_T(KC_ESC),
   MO(CODER), KC_N,   KC_M,      KC_COMMA,   KC_DOT,  KC_SLSH,   KC_RSHIFT,
                      LEFT_PANE, RIGHT_PANE, UP_PANE, DOWN_PANE, KC_RGUI,
