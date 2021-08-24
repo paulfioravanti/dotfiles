5c6b41f33f3548c4b3e998270072910b7a7aa811
Paul Fioravanti <paul.fioravanti@gmail.com>
2021-08-24 19:42:52 +1000

    Change ohmyzsh setup script command

--- oh-my-zsh/setup.sh
+++ oh-my-zsh/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Install oh-my-zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
