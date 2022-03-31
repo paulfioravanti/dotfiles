b0a9d8023919c982011b6674775e22b778436656
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-03-31 16:13:27 +1100

    Add deno and php to list of asdf plugins

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -4,6 +4,7 @@
 # Install all used plugins
 asdf plugin add clojure https://github.com/halcyon/asdf-clojure.git
 asdf plugin add crystal https://github.com/marciogm/asdf-crystal.git
+asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
 asdf plugin add dhall https://github.com/aaaaninja/asdf-dhall.git
 asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin add elm https://github.com/asdf-community/asdf-elm.git
@@ -14,6 +15,7 @@ asdf plugin add haskell https://github.com/vic/asdf-haskell.git
 asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin add julia https://github.com/rkyleg/asdf-julia.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add php https://github.com/asdf-community/asdf-php.git
 asdf plugin add python https://github.com/tuvistavie/asdf-python.git
 asdf plugin add redis https://github.com/smashedtoatoms/asdf-redis.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
