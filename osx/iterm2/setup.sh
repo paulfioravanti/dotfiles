efa16be1a1b6b7969f9fd4902dc4a221dbea5426
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-06-04 20:08:29 +1000

    Comment out iterm2 config that uses plists for now...

--- osx/iterm2/setup.sh
+++ osx/iterm2/setup.sh
@@ -8,6 +8,6 @@ GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
 echo "${YELLOW}Copying iterm2 config...${RESET}"
-cp "$ITERM_DIR/COM.googlecode.iterm2.plist" \
-  ~/Library/Preferences/com.googlecode.iterm2.plist
+# cp "$ITERM_DIR/COM.googlecode.iterm2.plist" \
+#   ~/Library/Preferences/com.googlecode.iterm2.plist
 echo "${GREEN}Complete!${RESET}"
